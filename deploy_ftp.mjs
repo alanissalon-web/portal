8374def2af0ce8ac007c00ce4f8963ce939d5089
Geeko18 <internauta509@gmail.com>
2026-06-22 15:51:54 -0500

    feat: add basic-ftp dependency and implement automated FTP deployment script

---
+++ deploy_ftp.mjs
@@ -0,0 +1,31 @@
+import fs from 'fs';
+import path from 'path';
+import * as ftp from 'basic-ftp';
+
+async function deploy() {
+    const client = new ftp.Client();
+    client.ftp.verbose = true;
+    try {
+        console.log("Connecting to FTP...");
+        await client.access({
+            host: "82.197.80.239",
+            user: "u873532103.alanissalon.com",
+            password: "@Alanis2026",
+            secure: false
+        });
+        
+        console.log("Connected! Uploading dist directory...");
+        const localDir = path.resolve('./dist');
+        
+        await client.ensureDir('public_html');
+        await client.uploadFromDir(localDir);
+        
+        console.log("Upload complete!");
+    }
+    catch(err) {
+        console.error("FTP Error:", err);
+    }
+    client.close();
+}
+
+deploy();
